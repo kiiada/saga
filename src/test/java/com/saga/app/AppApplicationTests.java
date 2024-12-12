80ce356614d4c08cb2110dc8fe2b0601e6ad776a
kiiada <maddie.groeling@gmail.com>
2024-12-12 15:47:29 -0700

    Initial commit

---
+++ src/test/java/com/saga/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saga.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
